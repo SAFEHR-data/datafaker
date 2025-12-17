d70f39ce9006f07dc61078e07409c2decda59d33
Tim Band <t.b@ucl>
2025-12-17 18:33:53 +0000

    Fixed test_dump

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -1,10 +1,10 @@
 """Tests for the base module."""
-import io
-from unittest.mock import MagicMock, call, patch
+import csv
+from pathlib import Path
+import tempfile
 
-from sqlalchemy.schema import MetaData
-
-from datafaker.dump import dump_db_tables
+from datafaker.dump import CsvTableWriter, get_parquet_table_writer
+import pandas as pd
 from tests.utils import RequiresDBTestCase
 
 
@@ -15,19 +15,35 @@ class DumpTests(RequiresDBTestCase):
     database_name = "instrument"
     schema_name = "public"
 
-    @patch("datafaker.dump._make_csv_writer")
-    def test_dump_data(self, make_csv_writer: MagicMock) -> None:
-        """Test dump-data."""
-        test_output_file = io.StringIO()
-        metadata = MetaData()
-        metadata.reflect(self.sync_engine)
-        dump_db_tables(metadata, self.dsn, self.schema_name, "player", test_output_file)
-        make_csv_writer.assert_called_once_with(test_output_file)
-        make_csv_writer.assert_has_calls(
-            [
-                call().writerow(["id", "given_name", "family_name"]),
-                call().writerow((1, "Mark", "Samson")),
-                call().writerow((2, "Tim", "Friedman")),
-                call().writerow((3, "Pierre", "Marchmont")),
-            ]
-        )
+    def test_dump_data_csv(self) -> None:
+        """Test dump-data for CSV output."""
+        outdir = Path(tempfile.mkdtemp("dump"))
+        writer = CsvTableWriter(self.metadata, self.dsn, None)
+        table_name = "manufacturer"
+        writer.write(self.metadata.tables[table_name], outdir)
+        table_path = outdir / f"{table_name}.csv"
+        self.assertTrue(table_path.is_file())
+        with table_path.open() as table_fh:
+            reader = csv.reader(table_fh)
+            content = list(reader)
+            self.assertListEqual(content[0], ["id", "name", "founded"])
+            self.assertListEqual(content[1], ["1", "Blender", "1951-01-08 12:05:06+00:00"])
+            self.assertListEqual(content[2], ["2", "Gibbs", "1959-03-04 15:08:09+00:00"])
+
+    def test_dump_data_parquet(self) -> None:
+        """Test dump-data for Parquet output."""
+        outdir = Path(tempfile.mkdtemp("dump"))
+        writer = get_parquet_table_writer(self.metadata, self.dsn, None)
+        table_name = "manufacturer"
+        writer.write(self.metadata.tables[table_name], outdir)
+        table_path = outdir / f"{table_name}.parquet"
+        self.assertTrue(table_path.is_file())
+        df = pd.read_parquet(table_path)
+        self.assertListEqual(df.columns.to_list(), ["id", "name", "founded"])
+        self.assertListEqual(df["id"].to_list(), [1, 2])
+        self.assertListEqual(df["name"].to_list(), ["Blender", "Gibbs"])
+        founded = [
+            pd.Timestamp("1951-01-08 12:05:06+00:00"),
+            pd.Timestamp("1959-03-04 15:08:09+00:00"),
+        ]
+        self.assertListEqual(df["founded"].to_list(), founded)
